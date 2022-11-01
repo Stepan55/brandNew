8889fc373b7696b12bd9738783c6d60b2b77c58c
Stepan <stepan_boynice@mail.ru>
2022-11-01 23:59:29 +0300

    Initial Commit

---
+++ from0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  from0
+//
+//  Created by Степан on 01.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
